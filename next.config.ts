8a746de7cb1b7532311439c84b28ef066a26e6b0
fansyah98 <fansyahdwi204@gmail.coom>
2025-12-15 13:38:52 +0700

    data

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
+  // menambahkan allowedDevOrigins untuk development mode
+  allowedDevOrigins: [
+    'http://localhost:3000',    // default dev
+    'http://192.168.1.2:3000',  // ganti sesuai IP device-mu
+  ],
 };
 
 export default nextConfig;
